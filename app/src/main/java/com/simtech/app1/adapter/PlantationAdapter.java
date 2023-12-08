c9651de0336c4fc92af32db0752034a2829bdf1b
PrakashYalavatti <prakashyalavatti21@gmail.com>
2023-12-08 15:09:34 +0530

    Demo changes

--- app/src/main/java/com/simtech/app1/adapter/PlantationAdapter.java
+++ app/src/main/java/com/simtech/app1/adapter/PlantationAdapter.java
@@ -97,7 +97,7 @@ public class PlantationAdapter extends RecyclerView.Adapter<PlantationAdapter.Vi
             public void afterTextChanged(Editable editable) {
                 if (editable.toString().length()>1) {
                     Double sample1 = Double.valueOf(editable.toString());
-                    if (sample1 <= 16) {
+                    if (sample1 < 16) {
                         DecimalFormat decimalFormat = new DecimalFormat("##.###");
                         // Format the number
                         String sample1Str = decimalFormat.format(sample1);
@@ -124,14 +124,14 @@ public class PlantationAdapter extends RecyclerView.Adapter<PlantationAdapter.Vi
             public void afterTextChanged(Editable editable) {
                 if (editable.toString().length()>1) {
                     Double sample2 = Double.valueOf(editable.toString());
-                    if (sample2 <= 16) {
+                    if (sample2 < 16) {
                         DecimalFormat decimalFormat = new DecimalFormat("##.###");
                         // Format the number
                         String sample1Str = decimalFormat.format(sample2);
-                        varietyDataPojo.setSample1(sample1Str);
+                        varietyDataPojo.setSample2(sample1Str);
                     } else {
-                        holder.etSample1.setError("Please enter value below 16");
-                        holder.etSample1.setText("");
+                        holder.etSample2.setError("Please enter value below 16");
+                        holder.etSample2.setText("");
                     }
                 }
             }
@@ -151,14 +151,14 @@ public class PlantationAdapter extends RecyclerView.Adapter<PlantationAdapter.Vi
             public void afterTextChanged(Editable editable) {
                 if (editable.toString().length()>1) {
                     Double sample3 = Double.valueOf(editable.toString());
-                    if (sample3 <= 16) {
+                    if (sample3 < 16) {
                         DecimalFormat decimalFormat = new DecimalFormat("##.###");
                         // Format the number
                         String sample1Str = decimalFormat.format(sample3);
-                        varietyDataPojo.setSample1(sample1Str);
+                        varietyDataPojo.setSample3(sample1Str);
                     } else {
-                        holder.etSample1.setError("Please enter value below 16");
-                        holder.etSample1.setText("");
+                        holder.etSample3.setError("Please enter value below 16");
+                        holder.etSample3.setText("");
                     }
                 }
             }
